320578f13b86da8164b6166c61f04f4848f1ead3
su <mz4ivo@gmail.com>
2019-10-22 17:23:30 +0800

    show real pubkey of validator pubkey

--- cmd/lambdadg/main.go
+++ cmd/lambdadg/main.go
@@ -1,8 +1,65 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/LambdaIM/lambda-debugtool/chain"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+const (
+	FlagPubkeyPrefix = "pubkey"
+	FalgBechPrefix   = "bech"
+)
+
+func showCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show",
+		Short: "show cons/real addr pubkey of valconspub address",
+		RunE:  runShowCmd,
+	}
+
+	cmd.Flags().String(FlagPubkeyPrefix, "", "The pubKey of lambda val account")
+	cmd.Flags().String(FalgBechPrefix, "pub", "The Bech32 prefix encoding for a key (pub|cons)")
+
+	if err := viper.BindPFlags(cmd.Flags()); err != nil {
+		panic(err)
+	}
+
+	return cmd
+}
+
+func runShowCmd(cmd *cobra.Command, args []string) error {
+	consPubkey := viper.GetString(FlagPubkeyPrefix)
+	if consPubkey == "" {
+		return cmd.Help()
+	}
+
+	pubkey, err := sdk.GetConsPubKeyBech32(consPubkey)
+	if err != nil {
+		return err
+	}
+
+	bech := viper.GetString(FalgBechPrefix)
+	switch bech {
+	case "pub":
+		{
+			fmt.Println(pubkey.Address())
+		}
+	case "cons":
+		{
+			addr := pubkey.Address().Bytes()
+			consAddr := sdk.ConsAddress(addr)
+			fmt.Println(consAddr.String())
+		}
+	default:
+		return fmt.Errorf("unknown bech type %v", bech)
+	}
+
+	return nil
+}
 
 func rootCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -14,8 +71,12 @@ func rootCmd() *cobra.Command {
 }
 
 func main() {
+	chain.SealConfig()
+
 	root := rootCmd()
+	root.AddCommand(showCmd())
+
 	if err := root.Execute(); err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module lambda-debugtool
+module github.com/LambdaIM/lambda-debugtool
 
 go 1.13
 
 require (
-	github.com/cosmos/cosmos-sdk v0.37.3
+	github.com/cosmos/cosmos-sdk v0.35.0
 	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.4.0
 )
